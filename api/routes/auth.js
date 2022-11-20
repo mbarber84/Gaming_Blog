ad291cff5e69c4810a3c2f623b8d829f490f4306
mbarber84 <mbarber_84@hotmail.co.uk>
2022-11-20 08:14:18 +0000

    login auth and hidden password

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const User = require("../models/Users");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 
 //Register
 router.post("/register", async (req, res) => {
   try {
-
-    const salt = await bcrypt.genSalt(10);//salting the password
-    const hashedPass = await bcrypt.hash(req.body.password, salt);//hashing the password
+    const salt = await bcrypt.genSalt(10); //salting the password
+    const hashedPass = await bcrypt.hash(req.body.password, salt); //hashing the password
 
     const newUser = new User({
       username: req.body.username,
@@ -23,5 +22,20 @@ router.post("/register", async (req, res) => {
 });
 
 //Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({username: req.body.username});
+    !user && res.status(400).json("Wrong Details Entered!");
+
+    const validated = await bcrypt.compare(req.body.password, user.password);
+    !validated && res.status(400).json("Wrong Details Entered!");
+
+    const {password, ...others} = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
