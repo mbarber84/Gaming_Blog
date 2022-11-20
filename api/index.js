c9b0e64c0293d548c5384ba57965a03d5af712d3
mbarber84 <mbarber_84@hotmail.co.uk>
2022-11-20 13:19:17 +0000

    API Completed

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -18,10 +20,26 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err)); //Arrow function
 
-  app.use("/api/auth", authRoute);
-  app.use("/api/users", userRoute);
-  app.use("/api/posts", postRoute);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
 
-app.listen("5000", () => { //Arrow function
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("You file has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  //Arrow function
   console.log("Backend is connected");
 });
