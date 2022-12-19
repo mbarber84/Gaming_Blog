f015e3ebfd327b9a638a353ecdac5b4d0099a1e1
mbarber84 <mbarber_84@hotmail.co.uk>
2022-12-19 10:14:47 +0000

    login page connected to context, backend

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ContextProvider } from "./context/Context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
 );
-
-
--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,19 +1,60 @@
-import { Link } from "react-router-dom"
-import "./login.css"
+import axios from "axios";
+import { useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./login.css";
 
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({type:"LOGIN_START"});
+    try{
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      })
+      dispatch({type:"LOGIN_SUCCESS", payload:res.data});
+    }catch(err){
+      dispatch({type:"LOGIN_FAILURE"});
+    }
+  };
+
+  console.log(user);
+
   return (
     <div className="login">
-        <span className="loginTitle">Login</span>
-        <form className="loginForm">
-            <label>Email</label>
-            <input type="email" className="loginInput" placeholder="Enter email address..."/>
-            <label>Password</label>
-            <input type="password" className="loginInput" placeholder="Enter password..."/>
-            <button className="loginBtn">Login</button>
-        </form>
-        <button className="loginRegisterBtn"><Link to="/register" style={{textDecoration:"none", color:"inherit"}}>Register</Link></button>
-        
+      <span className="loginTitle">Login</span>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter Username..."
+          ref={userRef}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="loginInput"
+          placeholder="Enter password..."
+          ref={passwordRef}
+        />
+        <button className="loginBtn" type="submit">
+          Login
+        </button>
+      </form>
+      <button className="loginRegisterBtn">
+        <Link
+          to="/register"
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          Register
+        </Link>
+      </button>
     </div>
-  )
+  );
 }
