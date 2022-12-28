4c856a46cb15488123bff44f1cc456ac552f8c8e
mbarber84 <mbarber_84@hotmail.co.uk>
2022-12-28 08:33:29 +0000

    Single comment

--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -2,6 +2,8 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.css";
 import SinglePost from "../../components/singlePost/SinglePost";
 
+// This code is a function called Single() that is exported as the default. It returns a div with the className "single" which contains two components, SinglePost and Sidebar. This function will be used to render the SinglePost and Sidebar components on the page.
+
 export default function Single() {
   return (
     <div className="single">
