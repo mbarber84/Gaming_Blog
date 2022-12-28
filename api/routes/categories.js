28bc1f7cd14ab798883400b787b00e8700db8fae
mbarber84 <mbarber_84@hotmail.co.uk>
2022-12-28 09:00:06 +0000

    categories comment

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const Category = require("../models/Category");
 
+//This code is a part of an Express.js router that handles requests for a Category resource. The first block of code is a POST request handler that creates a new Category object from the request body and saves it to the database. The second block of code is a GET request handler that retrieves all categories from the database and sends them back in the response. Finally, the router is exported so it can be used in other parts of the application.
+
 router.post("/", async (req, res)=> {
   const newCat = new Category(req.body);
   try {
